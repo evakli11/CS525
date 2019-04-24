b154ce77246fdbdaa621c284c0dc74befe9f766d
BernieYe <bony960323@gmail.com>
2019-04-24 13:59:52 -0400

    Update GUI.py
    Final Version

--- GUI.py
+++ GUI.py
@@ -1,62 +1,68 @@
-import tkinter as tk
+from tkinter import *
 import search_training as st
 import tkinter.ttk as ttk
 import pandas as pd
+import final
+from PIL import ImageTk, Image
+
+def insert_point():
+    keywords = kw_input.get()
+    sentiments = sentiment.get()
+    index_set,sentiments= search.search_indexes(keywords,sentiments)
+    data = pd.read_csv('processed_lyrics.csv')
+    training_data = data[(data['index'].isin(list(index_set))) & (data['sentiment'].isin(sentiments))]
+    if len(training_data) > 500:
+        training_data = training_data.sample(n=500, axis=0)
+    training_data['lyrics'].to_csv('training_lyrics.txt', index=False)
+    #-----train and generating process----#
+    n = final.neuralRapper("kanye_west",'neural_rap.txt')
+    n.main("kanye_west", 'neural_rap.txt', "training_lyrics.txt")
+    #self.lyric.insert('insert', sentiments)
+    lyric.config(state='normal')
+    lyric.delete("1.0", "end")
+    with open('neural_rap.txt','r') as f:
+        lyrics = f.read()
+        print(lyrics)
+    lyric.insert('insert', lyrics)
+    lyric.config(state='disabled')
+
+def play():
+    import speech
+        
+root = Tk()
+root.title('Rap Maker')
+root.geometry('500x700')
+canv = Canvas(root, width=500, height=700, bg='white')
+canv.place(x=0,y=0)
+img = ImageTk.PhotoImage(Image.open("bgfullsmall.jpg"))  # PIL solution
+canv.create_image(250, 350, anchor='center', image=img)
+
+#label1 = Label(root, text='Insert Keywords:', font=('Helvetica', 14), width=15, height=1)
+#label1.place(x=80,y=100)
+
+kw_input = Entry(root)
+kw_input.place(x=200,y=103)
+
+#label2 = Label(root, text='Choose Sentiment:', font=('Helvetica', 14), width=15, height=1).place(x=80,y=150)##
+
+sentiment = StringVar()
+sent = ttk.Combobox(root, textvariable=sentiment,width=12,state='readonly')
+sent["values"] = ("Positive","Negative", "All")
+sent.current(0)
+sent.place(x=220,y=153)
+
+extend1 = Button(root, text="Generate", width=15, height=2, command=insert_point)
+extend1.place(x=170,y=240)
+
+#label3 = Label(root, text='Generated Lyrics:', font=('Helvetica', 14), width=15, height=2).place(x=170,y=280)
 
 
-class App:
-    def __init__(self,master):
-        self.label1 = tk.Label(window, text='Insert Keywords:', font=('Helvetica', 14), width=15, height=2).place(x=80,y=100)
+lyric = Text(root,width=70, height=18, bg = '#FEF5E7',state='disabled')
+lyric.place(x=0,y=355)
 
-        self.kw_input = tk.Entry(window)
-        self.kw_input.place(x=200,y=103)
+extend2 = Button(root, text="Play MP3", width=15, height=2, command = play)
+extend2.place(x=170, y=650)
 
-        self.label2 = tk.Label(window, text='Choose Sentiment:', font=('Helvetica', 14), width=15, height=2).place(x=80,y=150)
+search = st.search()
 
-        self.sentiment = tk.StringVar()
-        self.sent = ttk.Combobox(window, textvariable=self.sentiment,width=12,state='readonly')
-        self.sent["values"] = ("Positive","Negative", "All")
-        self.sent.current(0)
-        self.sent.place(x=220,y=153)
-
-        self.extend1 = tk.Button(window, text="Generate", width=15, height=2, command=self.insert_point)
-        self.extend1.place(x=170,y=240)
-
-        self.label3 = tk.Label(window, text='Generated Lyrics:', font=('Helvetica', 14), width=15, height=2).place(x=170,y=280)
-
-
-        self.lyric = tk.Text(window,width=70, height=30, bg = '#FEF5E7',state='disabled')
-        self.lyric.place(x=0,y=320)
-
-	# self.extend2 = tk.Button(window, text="Download MP3", width=15, height=2, command=self.download)
-        # self.extend2.place(x=170, y=650)
-
-        self.search = st.search()
-
-   
-    # def download(self):
-
-
-    def insert_point(self):
-        keywords = self.kw_input.get()
-        sentiments = self.sentiment.get()
-        index_set,sentiments= self.search.search_indexes(keywords,sentiments)
-        data = pd.read_csv('processed_lyrics.csv')
-        training_data = data[(data['index'].isin(list(index_set))) & (data['sentiment'].isin(sentiments))]
-        if len(training_data) > 500:
-            training_data = training_data.sample(n=500, axis=0)
-        training_data['lyrics'].to_csv('training_lyrics.txt', index=False)
-        #-----train and generating process----#
-
-
-        self.lyric.config(state='normal')
-	self.lyric.delete("1.0", "end")
-        self.lyric.insert('insert', sentiments)
-        self.lyric.config(state='disabled')
-
-
-window = tk.Tk()
-window.title('DS595 Rapper Maker')
-window.geometry('500x700')  # window size
-display = App(window)
-window.mainloop()
+mainloop()
